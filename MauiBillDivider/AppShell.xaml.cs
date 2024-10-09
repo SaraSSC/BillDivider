acc4ed9fc8562b680868e1dc72fb3f4935fa0aa5
Sattineez_SC <83277661+SaraSSC@users.noreply.github.com>
2024-10-09 03:08:01 +0100

    Finishing the application

---
+++ MauiBillDivider/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBillDivider
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiBillDivider/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiBillDivider
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
